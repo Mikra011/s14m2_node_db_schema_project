b1ba00b37b0a512eee8fafbf1fa1c68a0c3f9acc
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-16 13:21:57 +0200

    Started

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,11 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1234567890123',
+        make: 'ford',
+        model: 'escort',
+        milage: 123000,
+        title: 'clean',
+        transmission: 'manual',
+    },
+]
