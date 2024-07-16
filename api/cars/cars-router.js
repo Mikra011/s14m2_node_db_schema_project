b0319444d6834e2770afce5855a0482417cd0a79
Robert Toth-Ludanyi <roberttothmails@yahoo.com>
2024-07-16 15:43:01 +0200

    endpoins working

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,18 @@
 // DO YOUR MAGIC
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    res.json('ok')
+})
+
+router.get('/:id', async (req, res, next) => {
+    res.json(`get by ${req.params.id} ok`)
+})
+
+router.post('/', async (req, res, next) => {
+    res.json('post new car')
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -7,10 +7,16 @@ const carsRouter = require('./cars/cars-router')
 // DO YOUR MAGIC
 server.use(express.json())
 
-// server.use('api/cars',carsRouter)
+server.use('/api/cars', carsRouter)
 
-server.get("/", (req, res) => {
-    res.status(200).json({ api: "up" })
+server.get("*", (req, res, next) => {
+    next({ status: 404, message: "not found" })
 })
+
+server.use((error, req, res, next) => { //eslint-disable-line
+    res.status(error.status || 500).json({
+        message: error.message,
+    });
+});
 
 module.exports = server
